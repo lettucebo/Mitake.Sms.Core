641bd1e3df04c7a927a363f728feb7d87cad79ec
Money Yu <abc12207@gmail.com>
2022-10-22 16:15:17 +0800

    add example project

---
+++ src/Aptg.KonaKart.Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Aptg.KonaKart.Example
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            Console.WriteLine("Hello, World!");
+            var smsService = new SmsService();
+            var connResult = await smsService.CreateConnectionAsync(config["SmsAccount"], config["SmsPassword"]).ConfigureAwait(false);
+
+            var sessionKey = connResult.Payload;
+
+            await smsService.CloseConnectionAsync(sessionKey).ConfigureAwait(false);
+        }
+    }
+}
